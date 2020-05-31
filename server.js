afb7bde7eb2723c12f154a2526963b581f60507d
The Farrier <nkfarrey@gmail.com>
2020-05-30 21:06:17 -0500

    changed connnect url to env

--- server.js
+++ server.js
@@ -15,11 +15,6 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
-
 // routes
 app.use(require("./routes/api.js"));
 
